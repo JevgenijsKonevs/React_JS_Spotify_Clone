54b010e29744de35ed29d8afbde6c382c20173bd
JevgenijsKonevs <jevgenijskonevs@gmail.com>
2020-10-16 16:04:12 +0300

    material-ui icons // SidebarOption component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,8 @@
 import React from "react";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryIcon from "@material-ui/icons/LibraryMusic";
 import "./Sidebar.css";
 function Sidebar() {
   return (
@@ -8,6 +12,12 @@ function Sidebar() {
         src={require("./img/spotify_logo_black_white.JPG")}
         alt=""
       />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryIcon} title="Your library" />
+      <br></br>
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr></hr>
     </div>
   );
 }

c12af7946d9d0b6c8a0260dd594825a30a27d384
JevgenijsKonevs <jevgenijskonevs@gmail.com>
2020-10-16 20:24:09 +0300

    Header component / css

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./Header";
 import "./Body.css";
-function Body() {
-  return <div className="body"></div>;
+function Body({ spotify }) {
+  return (
+    <div className="body">
+      <Header spotify={spotify} />
+      <div className="body__info">
+        <img src="" alt="" />
+        <div className="body__infoText"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Body;

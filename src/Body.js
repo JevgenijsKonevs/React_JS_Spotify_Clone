fb3f9615a1ecc255a2ad0668d32559bf9b687371
JevgenijsKonevs <jevgenijskonevs@gmail.com>
2020-10-16 22:22:41 +0300

    Song ron component implementation

--- src/Body.js
+++ src/Body.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "./Header";
 import { useDataLayerValue } from "./DataLayer";
+import PlayIcon from "@material-ui/icons/PlayCircleOutline";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreIcon from "@material-ui/icons/MoreHoriz";
+import SongRow from "./SongRow";
 import "./Body.css";
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
@@ -14,6 +18,16 @@ function Body({ spotify }) {
           <h2>Discover weekly</h2>
           <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreIcon />
+        </div>
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  token:
-    "BQCWXRdhXRk6E9ohfA7HoOhgN8Xi7N_ih68QI9ZbO74i3a5RaQ0fcdy-3dwYi25t0UZ6PkqTk8GEcI_h1K47Nby513Rax0TJl0z6mU989dAtGBtTstniVEO-C6B2GFBuYX8tSexFIYWGUhcSXTdw1VXwT_9MeI56IsFPSj-GZAuyDxrW",
+  //token:
+  //"BQCWXRdhXRk6E9ohfA7HoOhgN8Xi7N_ih68QI9ZbO74i3a5RaQ0fcdy-3dwYi25t0UZ6PkqTk8GEcI_h1K47Nby513Rax0TJl0z6mU989dAtGBtTstniVEO-C6B2GFBuYX8tSexFIYWGUhcSXTdw1VXwT_9MeI56IsFPSj-GZAuyDxrW",
 };
 const reducer = (state, action) => {
   // set the props from initialState to the following states, based on the action
